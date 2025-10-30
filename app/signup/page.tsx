e0e051252240ad056b91f62f9b226d67b90db7fe
omsaidesaii <omsaidesai9@gmail.com>
2025-10-30 21:12:04 +0530

    Refactor signup page and form components to enhance user experience. Updated layout, integrated form validation with Zod, and improved submission handling with loading states and error messages.

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,11 +1,11 @@
-import { SignupForm } from "@/components/forms/login-form"
+import { SignupForm } from "@/components/forms/signup-form";
 
-export default function LoginPage() {
+export default function Page() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-4xl">
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
         <SignupForm />
       </div>
     </div>
-  )
+  );
 }
